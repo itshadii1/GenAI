0f222150d0287318cbffcc03f9493491d9464901
itshadii1 <mahmoodhadi974@gmail.com>
2025-06-29 15:22:20 +0530

    Prompts component

---
+++ LangChain_prompts/chatbot.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Intelligent-Internet/II-Medical-8B-1706",
+    task="text-generation",
+    
+)
+model = ChatHuggingFace(llm=llm)
+
+chat_history = [
+    SystemMessage(content='You are a helpful AI assisstant')
+
+]
+#This chatbot is Stateful
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print('AI: ',result.content)
+print(chat_history)
--- LangChain_prompts/prompts_ui.py
+++ LangChain_prompts/prompts_ui.py
@@ -1,5 +1,4 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import PromptTemplate, load_prompt
@@ -14,12 +13,7 @@ style_input = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "Te
 length_input = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"])
 
 template = load_prompt('template.json')
-#fill the placeholders
-prompt = template.invoke({
-    'paper_input': paper_input,
-    'style_input': style_input,
-    'length_input': length_input
-})
+
 llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1",
     task="text-generation",
@@ -28,5 +22,10 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 if st.button('Summarize'):
-    result = model.invoke(prompt)
+    chain = template | model
+    result = chain.invoke({
+    'paper_input': paper_input,
+    'style_input': style_input,
+    'length_input': length_input
+    })
     st.write(result.content)
