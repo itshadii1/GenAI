0ce04aeede33b1f4460baaabfcd41e2aba763f60
itshadii1 <mahmoodhadi974@gmail.com>
2025-07-08 22:29:27 +0530

    Updated Runnables

---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,34 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_ollama.chat_models import ChatOllama
+
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOllama(
+    model="llama3.2:latest",
+    base_url="http://localhost:11434",
+    temperature=0,
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
